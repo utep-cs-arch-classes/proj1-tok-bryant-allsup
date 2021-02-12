6dda23bd7e67ee15de49d976d182c270dedc9c46
bryant-allsup <baallsup@miners.utep.edu>
2021-02-12 13:08:12 -0700

    added space_char and non_space_char functionality to tokenizer.c

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -2,7 +2,15 @@
 #define "tokenizer.h"
 
 int space_char(char c)
-{}
+{
+  if(c == ' '||'\t'||'\n')
+    return 1;
+  return 0;
+}
 
 int non_space_char(char c)
-{}
+{
+  if(c == ' '||'\t'||'\n')
+    return 0;
+  return 1;
+}
