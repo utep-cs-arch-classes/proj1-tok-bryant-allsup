ec4b03d5c0239abbf06bcc8c0d92a191eb9e80f9
bryant-allsup <baallsup@miners.utep.edu>
2021-02-12 12:35:35 -0700

    redid ui.c, was not assigning to c before

--- src/ui.c
+++ src/ui.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
-
+#define limit 100
 
 void main()
 {
+  int count = 0;
+  char word[limit];
   char c;
-  printf("*");
-  while(c = getchar() != '\n')
-  {
-    putchar(c);
+  while(1){
+    while((c = getchar()) != '\n')
+      {
+	word[count] =putchar(c);
+	count++;
+      }
+    printf("Printing Word: %s",word);
+    printf("\n");
   }
 }
