6bd363eacb7b5cc44a710de36b9d09097a5ef5f5
bryant-allsup <baallsup@miners.utep.edu>
2021-02-20 11:44:31 -0700

    added start, end and count words functions

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,16 +1,31 @@
-#define "ui.c"
-#define "tokenizer.h"
+#include <stdio.h>
+#include "tokenizer.h"
 
-int space_char(char c)
-{
-  if(c == ' '||'\t'||'\n')
-    return 1;
+int space_char(char c){ 
+  if(c == ' ' || c == '\t' || c == '\n')
+    {return 1;}
   return 0;
 }
 
-int non_space_char(char c)
-{
-  if(c == ' '||'\t'||'\n')
-    return 0;
+int non_space_char(char c){
+  if(c == ' ' || c == '\t' || c == '\n')
+    {return 0;}
   return 1;
+}
+
+//str is assumed to be pointing to a space char.
+char *word_start(char *str){ //does not increment through word
+  while(space_char(*str)){ //no longer space
+    str++; //increment pointer
+    }
+  return *str;
+    
+}
+//str is assumed to be pointing to a non space char
+char *word_end(char *str){
+  while()
+}
+
+int count_words(char *str){
+  
 }
--- src/ui.c
+++ src/ui.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "tokenizer.c"
 #define limit 100
 
 void main()
@@ -6,13 +7,21 @@ void main()
   int count = 0;
   char word[limit];
   char c;
-  while(1){
+  printf("*");
     while((c = getchar()) != '\n')
-      {
-	word[count] =putchar(c);
+      { 
+	word[count] = putchar(c);
+
+   
+	if( space_char(c) )
+	  {printf("This is a space\n");}
+
+	if(non_space_char(c) )
+	  {printf(":This is not a space\n");}
+
 	count++;
+	
       }
     printf("Printing Word: %s",word);
     printf("\n");
-  }
 }
